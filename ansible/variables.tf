916d70586f90860d4fe3cd6574e4a78efa61e8b9
Isaiah Herr <herrx080@umn.edu>
2024-10-25 15:12:28 -0500

    Added approle and kvv2 documentation and examples

---
+++ ansible/variables.tf
@@ -0,0 +1,11 @@
+variable "vault_address" {
+  type        = string
+  description = "Vault address"
+  default     = "https://hcp-vault-private-vault-fc507e0d.5d5b1f21.z1.hashicorp.cloud:8200"
+}
+
+variable "namespace" {
+  type        = string
+  description = "Prefix with admin/ (ex: admin/secm, admin/secm/subteamA)"
+  # default     = "admin/CESI"
+}
